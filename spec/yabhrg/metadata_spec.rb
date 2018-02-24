dbb52d84a2569dd1d01f0a0ecf7aaec6c5948c4b
Jose Galisteo <ceritium@gmail.com>
2018-02-24 23:57:00 +0100

    More metadata methods, remove useless file

--- spec/yabhrg/metadata_spec.rb
+++ spec/yabhrg/metadata_spec.rb
@@ -1,40 +1,18 @@
 RSpec.describe Yabhrg::Metadata do
   subject(:instance) { described_class.new(api_key: "foo", subdomain: "bar") }
 
-  let(:fields_response) do
-    YAML.load_file("spec/support/webmock/metadata_fields.yml")
-  end
-
-  let(:tables_response) do
-    File.read("spec/support/webmock/metadata_tables.xml")
-  end
-
-  let(:lists_response) do
-    File.read("spec/support/webmock/metadata_lists.json")
-  end
-
-  def stub_request_fields
-    url = "https://api.bamboohr.com/api/gateway.php/bar/v1/meta/fields"
-
-    stub_request(:get, url).
-      to_return(status: 200, body: fields_response.to_json, headers: {})
-  end
-
-  def stub_request_tables
-    url = "https://api.bamboohr.com/api/gateway.php/bar/v1/meta/tables"
-
-    stub_request(:get, url).
-      to_return(status: 200, body: tables_response, headers: {})
-  end
-
-  def stub_request_lists
-    url = "https://api.bamboohr.com/api/gateway.php/bar/v1/meta/lists"
-
-    stub_request(:get, url).
-      to_return(status: 200, body: lists_response, headers: {})
-  end
-
   describe "#fields" do
+    let(:fields_response) do
+      YAML.load_file("spec/support/webmock/metadata_fields.yml")
+    end
+
+    let(:stub_request_fields) do
+      url = "https://api.bamboohr.com/api/gateway.php/bar/v1/meta/fields"
+
+      stub_request(:get, url).
+        to_return(status: 200, body: fields_response.to_json, headers: {})
+    end
+
     it "parse json" do
       stub_request_fields
       expect(instance.fields).to eq(fields_response)
@@ -58,6 +36,17 @@ RSpec.describe Yabhrg::Metadata do
   end
 
   describe "#tables" do
+    let(:tables_response) do
+      File.read("spec/support/webmock/metadata_tables.xml")
+    end
+
+    let(:stub_request_tables) do
+      url = "https://api.bamboohr.com/api/gateway.php/bar/v1/meta/tables"
+
+      stub_request(:get, url).
+        to_return(status: 200, body: tables_response, headers: {})
+    end
+
     it "parse xml" do
       stub_request_tables
       tables = instance.tables
@@ -96,6 +85,17 @@ RSpec.describe Yabhrg::Metadata do
   end
 
   describe "#lists" do
+    let(:lists_response) do
+      File.read("spec/support/webmock/metadata_lists.json")
+    end
+
+    let(:stub_request_lists) do
+      url = "https://api.bamboohr.com/api/gateway.php/bar/v1/meta/lists"
+
+      stub_request(:get, url).
+        to_return(status: 200, body: lists_response, headers: {})
+    end
+
     it "parse json" do
       stub_request_lists
       expect(instance.lists.to_json).to eq(JSON.parse(lists_response).to_json)
@@ -161,6 +161,112 @@ RSpec.describe Yabhrg::Metadata do
         }
       }
       expect(response).to eq(expected_response)
+    end
+  end
+
+  describe "#time_off_types" do
+    let(:time_off_types_response) do
+      File.read("spec/support/webmock/metadata_time_off_types.json")
+    end
+
+    let(:stub_request_time_off_types) do
+      url = "https://api.bamboohr.com/api/gateway.php/bar/v1/meta/time_off/types"
+
+      stub_request(:get, url).
+        to_return(status: 200, body: time_off_types_response, headers: {})
+    end
+
+    it "parse json" do
+      stub_request_time_off_types
+      expect(instance.time_off_types.to_json).
+        to eq(JSON.parse(time_off_types_response).to_json)
+    end
+
+    it "memoize request" do
+      stub = stub_request_time_off_types
+
+      instance.time_off_types
+      instance.time_off_types
+      expect(stub).to have_been_requested.once
+    end
+
+    it "force request" do
+      stub = stub_request_time_off_types
+
+      instance.time_off_types
+      instance.time_off_types(true)
+      expect(stub).to have_been_requested.twice
+    end
+  end
+
+  describe "#time_off_policies" do
+    let(:time_off_policies_response) do
+      File.read("spec/support/webmock/metadata_time_off_policies.json")
+    end
+
+    let(:stub_request_time_off_policies) do
+      url = "https://api.bamboohr.com/api/gateway.php/bar/v1/meta/time_off/policies"
+
+      stub_request(:get, url).
+        to_return(status: 200, body: time_off_policies_response, headers: {})
+    end
+
+    it "parse json" do
+      stub_request_time_off_policies
+      expect(instance.time_off_policies.to_json).
+        to eq(JSON.parse(time_off_policies_response).to_json)
+    end
+
+    it "memoize request" do
+      stub = stub_request_time_off_policies
+
+      instance.time_off_policies
+      instance.time_off_policies
+      expect(stub).to have_been_requested.once
+    end
+
+    it "force request" do
+      stub = stub_request_time_off_policies
+
+      instance.time_off_policies
+      instance.time_off_policies(true)
+      expect(stub).to have_been_requested.twice
+    end
+  end
+
+  describe "#users" do
+    let(:response_body) do
+      File.read("spec/support/webmock/metadata_users.json")
+    end
+
+    let!(:stub_request_api) do
+      url = "https://api.bamboohr.com/api/gateway.php/bar/v1/meta/users"
+
+      stub_request(:get, url).
+        to_return(status: 200, body: response_body, headers: {})
+    end
+
+    it "parse json" do
+      stub_request_api
+
+      expect(instance.users.to_json).
+        to eq(JSON.parse(response_body).to_json)
+    end
+
+    it "memoize request" do
+      stub = stub_request_api
+
+      instance.users
+      instance.users
+      expect(stub).to have_been_requested.once
+    end
+
+    it "force request" do
+      stub = stub_request_api
+
+      instance.users
+      instance.users(true)
+      expect(stub).to have_been_requested.twice
     end
   end
 end
